30ed81b16583c4054871028e3e6574e2b14dea90
Sai Lohitha <Sai Lohitha@DESKTOP-JQTQ84R>
2020-07-22 21:52:15 +0530

    rask1 finishw=ed

---
+++ epamtask1/src/main/java/Epam/epamtask1/clone.java
@@ -0,0 +1,10 @@
+package Epam.epamtask1;
+
+public class clone {
+	
+
+		   public static void main(String[] args) {
+		      System.out.println("Welcome to the task1 of epam which is sharing the java projects into the github !!");
+		   }
+}
+
